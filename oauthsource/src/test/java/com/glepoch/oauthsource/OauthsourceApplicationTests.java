e26d675bac20d7aad6825b63764b4a2e71af710e
GLepoch <537073101@qq.com>
2020-09-25 17:39:46 +0800

    第一版基础项目提交：springclous+oauth2+jwt

---
+++ oauthsource/src/test/java/com/glepoch/oauthsource/OauthsourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glepoch.oauthsource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthsourceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
