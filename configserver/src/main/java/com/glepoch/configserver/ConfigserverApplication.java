1f2110fc87327320d759e44b092799c4a04c6d1d
GLepoch <537073101@qq.com>
2020-09-26 20:08:24 +0800

    加入config

--- configserver/src/main/java/com/glepoch/configserver/ConfigserverApplication.java
+++ configserver/src/main/java/com/glepoch/configserver/ConfigserverApplication.java
@@ -2,7 +2,10 @@ package com.glepoch.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableConfigServer
@@ -11,5 +14,4 @@ public class ConfigserverApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConfigserverApplication.class, args);
     }
-
 }
